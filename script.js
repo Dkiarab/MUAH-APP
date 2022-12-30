ca17dd66e05baf07eb5d986afbabc216373ed3c7
donna booker <donnabooker444@gmail.com>
2022-12-30 10:21:16 -0500

    finshedHomepage

--- script.js
+++
@@ -1,8 +0,0 @@
-var pinkmenu = document.querySelector(".pinkmenu");
-var sidebar = document.querySelector(".sidebar");
-var container = document.querySelector(".container");
-
-pinkmenu.onclick = function () {
-  sidebar.classList.toggle("small-sidebar");
-  conatainer.classList.toggle("large-container");
-};
